cb0eb72d61c672356f622aeadebb1ec4974b9e76
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-30 17:38:06 +0900

    change 'title' -> title

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -11,7 +11,7 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text('title'),
+      child: Text(title),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
