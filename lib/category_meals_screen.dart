4e2cb910cf54e90021185755a32d7935aa83b148
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-30 18:23:44 +0900

    pass data when routing by using constructor

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
 
+  final String categoryId;
+  final String categoryTitle;
+
+  const CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
+        title: Text(categoryTitle),
       ),
 
       body: Center(
