db3bbc15f2a5c1ee03efea75f96b30de6d742180
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-31 18:16:35 +0900

    remove appBar of CategoriesScreen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,10 +7,6 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals')
-      ),
-
       body: GridView(
         padding: const EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
